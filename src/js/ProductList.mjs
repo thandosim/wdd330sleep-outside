a6542e530ecc9347c595338e08c67a5df08e43dd
thandosim <sim24001@byui.edu>
2025-05-16 23:24:26 +0200

    created product card template

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,4 +1,19 @@
-
+function productCardTemplate(product) {
+    const productListItem = html`
+        <li class="product-card">
+            <a href="product_pages/?product=">
+              <img
+                src=""
+                alt=""
+              />
+              <h3 class="card__brand"></h3>
+              <h2 class="card__name"></h2>
+              <p class="product-card__price">$</p>
+            </a>
+        </li>
+    `;
+    return productListItem;
+}
 
 
 export default class ProductList {
--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,5 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./js/ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const productList = new ProductList("tents", dataSource, document.querySelector(".productList"));
+const productList = new ProductList("tents", dataSource, document.querySelector(".product-list"));
 productList.init();
