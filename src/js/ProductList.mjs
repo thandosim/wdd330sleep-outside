7ec4dc1ee99dc41df79c9843bf9e7f97a33918da
thandosim <sim24001@byui.edu>
2025-05-16 23:12:29 +0200

    starting w2 individual activity

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,14 @@
+
+
+
+export default class ProductList {
+    constructor(category, datasource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+    }
+}
