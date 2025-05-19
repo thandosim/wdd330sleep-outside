af71f18c705c0ca1f38fbb440912d63db54a5518
oni1997 <dzidzaimaenza@gmail.com>
2025-05-19 14:19:47 +0200

    Add ProductList class and main.js functionality for product rendering

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+
 const dataSource = new ProductData("tents");
-const productList = new ProductList(
-  "tents",
-  dataSource,
-  document.querySelector(".product-list"),
-);
-loadHeaderFooter();
+const productList = new ProductList("tents", dataSource, document.querySelector(".productList"));
 productList.init();
