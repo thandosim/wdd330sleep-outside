82bfce7c9c4730efe5728c448062f99e4cec68e2
IsraelMobote <moboteisrael02@gmail.com>
2025-05-20 05:36:33 +0100

    worked on the team activity for week2

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,10 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   let cartItems = getLocalStorage("so-cart") || [];
