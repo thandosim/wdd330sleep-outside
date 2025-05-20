1714fce99adf301ec7d38217694a6a0d624e5a6e
IsraelMobote <moboteisrael02@gmail.com>
2025-05-20 07:21:04 +0100

    edits on rendering product details to product-pages>index.html

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -6,17 +6,27 @@ export default class ProductDetails {
     }
 
     async init() {
-        const ProductDetails = await this.dataSource.findProductById(this.productId);
+        this.product = await this.dataSource.findProductById(this.productId);
         this.renderProductDetails();
+
+        document.getElementById("addToCart").addEventListener('click', this.addProductToCart.bind(this));
     }
 
     addProductToCart(product) {
         let cartItems = getLocalStorage("so-cart") || [];
-        cartItems.push(product);
+        cartItems.push(this.product);
         setLocalStorage("so-cart", cartItems);
     }
 
     renderProductDetails() {
-
+        const productDetailSection = document.querySelector('.product-detail');
+        productDetailSection.children[0].textContent = this.product.Brand.Name;
+        productDetailSection.children[1].textContent = this.product.NameWithoutBrand;
+        productDetailSection.children[2].src = this.product.Image;
+        productDetailSection.children[2].alt = this.product.NameWithoutBrand;
+        productDetailSection.getElementsByClassName(product-card__price).textContent = `$${this.product.ListPrice}`;
+        productDetailSection.getElementsByClassName(product__color).textContent = this.product.Colors.ColorName;
+        productDetailSection.getElementsByClassName(product__description).innerHTML = this.product.DescriptionHtmlSimple;
     }
 }
+
