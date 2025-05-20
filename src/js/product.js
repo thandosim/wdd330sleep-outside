79b14f0b4975f34632aeba428c41126d21599722
IsraelMobote <moboteisrael02@gmail.com>
2025-05-20 05:40:12 +0100

    edit

--- src/js/product.js
+++ src/js/product.js
@@ -2,7 +2,7 @@ import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 
 console.log(dataSource.findProductById(productId));
 
