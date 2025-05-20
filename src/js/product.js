2ed5866736850b33bd5ddb3de3de522067e63a87
thandosim <sim24001@byui.edu>
2025-05-20 22:18:39 +0200

    cleaned up old code and fixed home button on header template

--- src/js/product.js
+++ src/js/product.js
@@ -9,23 +9,3 @@ const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 loadHeaderFooter()
 product.init();
-
-// console.log(dataSource.findProductById(productId));
-// const testing = dataSource.findProductById(productId);
-// setLocalStorage("testing1", testing);
-
-// function addProductToCart(product) { //function moved to ProductDetails.mjs
-//   let cartItems = getLocalStorage("so-cart") || [];
-//   cartItems.push(product);
-//   setLocalStorage("so-cart", cartItems);
-// }
-// add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
