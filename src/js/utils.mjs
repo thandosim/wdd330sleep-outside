8558e419b7c33816e991872737a1b8e05f7c8614
thandosim <sim24001@byui.edu>
2025-05-20 22:03:17 +0200

    fixed minor issues after fully implememting partials for header and footer for every page

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -58,7 +58,7 @@ export async function loadHeaderFooter() {
   const headerElement = document.querySelector("#main-header");
   renderWithTemplate(headerTemplate, headerElement);
 
-  const footerTemplate = await loadTemplate("../partials/header.html");
-  const footerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
   renderWithTemplate(footerTemplate, footerElement);
 }
