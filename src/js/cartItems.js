2803ac83e87a3efaa7e1094a2e98e4710605b42e
IsraelMobote <moboteisrael02@gmail.com>
2025-05-22 05:18:25 +0100

    final edit on duplicate cart Item

--- src/js/cartItems.js
+++ src/js/cartItems.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 
 export default function cartItemTemplate(item) {
    return `<p class="item-${item.Id}">${item.NameWithoutBrand} <span>Quantity ${getLocalStorage(item.Name)}<span></p>`
--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,7 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import CartItem from "./cartItems";
+
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
