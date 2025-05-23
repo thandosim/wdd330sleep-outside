8db07fa3b8f158e4344e2c8e30cfcc26fd6142fe
thandosim <sim24001@byui.edu>
2025-05-23 22:43:52 +0200

    after lint, format and build

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,15 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { getParam, loadHeaderFooter, capitalizeWords } from "./utils.mjs";
-const category = getParam('category');
+const category = getParam("category");
 const title = document.querySelector("#category");
-title.textContent += `${capitalizeWords(category)}`
+title.textContent += `${capitalizeWords(category)}`;
 const listElement = document.querySelector(".product-list");
 const dataSource = new ProductData();
-const myList = new ProductList(
-  category,
-  dataSource,
-  listElement,
-);
+const myList = new ProductList(category, dataSource, listElement);
 loadHeaderFooter();
 myList.init();
