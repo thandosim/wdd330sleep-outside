5b6e97ca59f5ed36d475c695c9798dcbd14770b4
lillianarp <87861783+lillianarp@users.noreply.github.com>
2025-06-01 14:46:45 +1200

    Adding dynamic alert system from JSON file (Trello individual activity)

---
+++ src/js/Alert.js
@@ -0,0 +1,24 @@
+export default class Alert { // i think this is how we do classes zz 
+  async init() {
+    const response = await fetch("/json/alerts.json");
+    const alertsArr = await response.json();
+
+    const section = document.createElement("section");
+    section.classList.add("alert-list");
+
+    console.log("Main found?", document.querySelector("main"));
+
+
+    alertsArr.forEach(a => {
+      const p = document.createElement("p");
+      p.textContent = a.message;
+      p.style.backgroundColor = a.background;
+      p.style.color = a.color;
+      p.style.border = "1px solid red";
+      section.appendChild(p);
+      console.log("Adding alert:", a.message); // i can't see it :( 
+    });
+
+    document.querySelector("main").prepend(section);
+  }
+}
