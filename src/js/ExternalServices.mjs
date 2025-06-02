ceeec6e8f6ac7160c9f554c5557f171265445fb0
thandosim <sim24001@byui.edu>
2025-06-02 06:44:23 +0200

    reformated response in convert to json to check after converting to json

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,12 +1,15 @@
 const baseURL = import.meta.env.VITE_SERVER_URL
 
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
+async function convertToJson(res) {
+    const jsonResponse = await res.json();
+
+    if (res.ok) {
+        return jsonResponse;
+    } else {
+        throw { name: "servicesError", message: jsonResponse };
+    }
 }
+
 
 export default class ExternalServices {
   constructor() {
