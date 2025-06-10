db9160ce362884d8de30adc4fd19a4240f846f30
thandosim <sim24001@byui.edu>
2025-06-10 21:39:38 +0200

    updated extenal convert to json

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,12 +1,16 @@
 const baseURL = import.meta.env.VITE_SERVER_URL
 
 async function convertToJson(res) {
-    const jsonResponse = await res.json();
-
-    if (res.ok) {
-        return jsonResponse;
-    } else {
-        throw { name: "servicesError", message: jsonResponse };
+    try {
+        const jsonResponse = await res.json();
+        if (res.ok) {
+            return jsonResponse;
+        } else {
+            throw { name: "servicesError", message: jsonResponse };
+        }
+    } catch (err) {
+        console.error("Error parsing JSON:", err);
+        throw new Error(`Invalid JSON response: ${err.message}`);
     }
 }
 
