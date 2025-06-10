6a2733011d63da7e26ead85a3fa6e0403e0037e9
thandosim <sim24001@byui.edu>
2025-06-10 19:52:34 +0200

    trying to fix header and footer in production

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+loadHeaderFooter();
 const dataSource = new ExternalServices("tents");
 const productList = new ProductList(
   "tents",
   dataSource,
   document.querySelector(".product-list"),
 );
-loadHeaderFooter();
+// loadHeaderFooter();
 productList.init();
